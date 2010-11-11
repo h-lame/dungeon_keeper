669722ea98ef495e3dc36a2e5bd8bfd34933bf3b
Murray Steele <muz@h-lame.com>
2010-11-11 10:33:38 +0000

    More helper methods and tests for them.

--- app/helpers/traps_helper.rb
+++ app/helpers/traps_helper.rb
@@ -1,2 +1,9 @@
 module TrapsHelper
+  def dungeons_a_trap_is_installed_in(for_trap)
+    if for_trap.dungeons.any?
+      for_trap.dungeons.map {|d| link_to d.name, d }.to_sentence
+    else
+      %Q{None! Why not #{link_to 'install it in one', new_trap_installation_path}}.html_safe
+    end
+  end
 end
--- test/unit/helpers/dungeons_helper_test.rb
+++ test/unit/helpers/dungeons_helper_test.rb
@@ -1,4 +1,18 @@
 require 'test_helper'
 
 class DungeonsHelperTest < ActionView::TestCase
+  test "show_evil_wizard_for_dungeon should return a link to the evil wizard for the dungeon" do
+    d = Dungeon.create!(:name => 'Despair' , :levels => 4)
+    e = d.create_evil_wizard(:name => 'David Blaine', :magic_school => 'stage', :experience_points => 450)
+
+    render :text => show_evil_wizard_for_dungeon(d)
+    assert_select 'a[href=?]', evil_wizard_path(e)
+  end
+
+  test "show_evil_wizard_for_dungeon should return a link to adding a new evil wizard if the dungeon doesn't have an evil wizard in it" do
+    d = Dungeon.create!(:name => 'Despair' , :levels => 4)
+    d.evil_wizard = nil
+    render :text => show_evil_wizard_for_dungeon(d)
+    assert_select 'a[href=?]', new_evil_wizard_path
+  end
 end
