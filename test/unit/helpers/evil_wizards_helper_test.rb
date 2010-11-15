0da8324677df06f0e7fc56623236a153d1aee73d
Murray Steele <muz@h-lame.com>
2010-11-16 00:41:40 +0200

    Adding factory_girl to the tests app.

--- test/unit/helpers/evil_wizards_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class EvilWizardsHelperTest < ActionView::TestCase
-end
--- test/unit/helpers/trap_installations_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class TrapInstallationsHelperTest < ActionView::TestCase
-end
