5d66c10370a72dacd1a43fac2bf3a57db2080bb8
Murray Steele <muz@h-lame.com>
2010-11-15 21:32:12 +0200

    Fixing up the helper tests into rspec format.

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe ApplicationHelper do
+  context "actions_menu" do
+    it "should wrap each argument in a <li> inside a single <ul> with the class 'actions'" do
+      actions_menu('whatever', 'i', 'want').should have_selector('ul.actions') do |scope|
+        scope.should have_selector('li', :content => 'whatever')
+        scope.should have_selector('li', :content => 'i')
+        scope.should have_selector('li', :content => 'want')
+      end
+    end
+
+    it "actions_menu should output nothing if no arguments are supplied" do
+      actions_menu.should be_blank
+    end
+  end
+
+  context "yes_or_no" do
+    it "should return 'yes' if the supplied argument is truthy" do
+      [true, '', :a_thing].each do |truthy_value|
+        yes_or_no(truthy_value).should == 'yes'
+      end
+    end
+
+    it "should return 'no' if the supplied argument is falsy" do
+      [false, nil].each do |falsy_value|
+        yes_or_no(falsy_value).should == 'no'
+      end
+    end
+  end
+end
